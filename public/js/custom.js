c822863a5ffb49275a5aa97059dafcb2fe4c9051
Yujing Gao <gao.yujing.csu@gmail.com>
2020-02-20 01:45:14 -0500

    theme changes

---
+++ public/js/custom.js
@@ -0,0 +1,5 @@
+function agreeTermOfService() {
+    document.getElementById("reserve-button").style.display = "none";
+    document.getElementById("pay-button").style.display = "inline";
+    document.getElementById("agreement-message").style.display = "inline";
+}
