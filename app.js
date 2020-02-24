215df2683745de15541e5df9873ca08ea70afaaf
Yujing Gao <gao.yujing.csu@gmail.com>
2020-02-24 01:11:56 -0500

    add helmet

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const http = require('http');
 const path = require('path');
+const helmet = require('helmet'); // for security consideration
 
 const express = require('express');
 let app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //Express serves images, CSS files, and JavaScript files in a directory named public
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 
 // Root GET URL takes the configuration file and render the index.ejs file
 app.get('/', function (req, res) {
